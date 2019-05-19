a08b1d93badf90b3f5e11e7f6ca1b4dc8845dd0e
KarthikM <karthik.medam1989@gmail.com>
2019-05-19 14:10:30 +0530

    img

--- cookbooks/starter/recipes/default.rb
+++ cookbooks/starter/recipes/default.rb
@@ -1,8 +1,16 @@
 # This is a Chef recipe file. It can be used to specify resources which will
 # apply configuration to a server.
 
-log "Welcome to Chef, #{node["starter_name"]}!" do
-  level :info
-end
+#log "Welcome to Chef, #{node["starter_name"]}!" do
+#  level :info
+#end
 
 # For more information, see the documentation: https://docs.chef.io/essentials_cookbook_recipes.html
+# Lets create a file
+file '/home/ubuntu' do
+  content 'This is a file'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
